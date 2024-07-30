9f5b86c691f1891dfd25ed267985a678b408b404
Pranay Raj <164135248+PranayRaj1007@users.noreply.github.com>
2024-07-30 14:45:15 +0530

    Update number_of_graded_assignments_for_each_student.sql

--- tests/SQL/number_of_graded_assignments_for_each_student.sql
+++ tests/SQL/number_of_graded_assignments_for_each_student.sql
@@ -1,1 +1,5 @@
 -- Write query to get number of graded assignments for each student:
+SELECT assignments.state AS "assignment state", 
+COUNT(*) AS "number of assignments" 
+from assignments 
+GROUP BY assignments.state;
