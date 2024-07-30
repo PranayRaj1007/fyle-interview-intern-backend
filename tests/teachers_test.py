d06eed8861a1409905a7d6f61a6a21be772be973
Pranay Raj <164135248+PranayRaj1007@users.noreply.github.com>
2024-07-30 14:50:47 +0530

    Update teachers_test.py

--- tests/teachers_test.py
+++ tests/teachers_test.py
@@ -9,6 +9,7 @@ def test_get_assignments_teacher_1(client, h_teacher_1):
     data = response.json['data']
     for assignment in data:
         assert assignment['teacher_id'] == 1
+        assert assignment['state'] in ['SUBMITTED', 'GRADED']
 
 
 def test_get_assignments_teacher_2(client, h_teacher_2):
@@ -99,3 +100,23 @@ def test_grade_assignment_draft_assignment(client, h_teacher_1):
     data = response.json
 
     assert data['error'] == 'FyleError'
+
+def test_grade_assignment_successful(client, h_teacher_1):
+    """
+    Success case: assignment 1 was submitted to teacher 1 successfully
+    """
+    response = client.post(
+        '/teacher/assignments/grade',
+        headers=h_teacher_1,
+        json={
+            "id": 1,
+            "grade": "A"
+        }
+    )
+
+    assert response.status_code == 200
+    data = response.json
+
+    assert data['data']['grade'] == 'A'
+    assert data['data']['state'] == 'GRADED'
+    assert data['data']['teacher_id'] == 1
